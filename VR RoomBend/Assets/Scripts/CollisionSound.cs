03c6dcfb59bdc3068c71b9701d540d6360b473b8
VeryBester <david15202@gmail.com>
2019-03-03 00:06:30 -0600

    added collides to a lot more things and changed scripts

--- VR RoomBend/Assets/Scripts/CollisionSound.cs
+++ VR RoomBend/Assets/Scripts/CollisionSound.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 namespace Assets.Scripts
 {
-    public class Interactions : MonoBehaviour
+    public class CollisionSound : MonoBehaviour
     {
+
         private void OnCollisionEnter(Collision other) {
             /*
             Will use to implement material interaction/pitch
@@ -13,9 +14,12 @@ namespace Assets.Scripts
 
             GameObject hitObject = other.gameObject;
             ScriptMaterial otherMat = hitObject.GetComponent<ScriptMaterial>();
-            if(otherMat != null){
+            if(hitObject.tag == "Reset"){
+
+            }
+            else if(otherMat != null){
                 Debug.Log(otherMat.name);
-                Debug.Log(otherMat.hardness);
+            
             }
 
             AudioSource audioSource = this.gameObject.GetComponent<AudioSource>(); 
@@ -25,6 +29,10 @@ namespace Assets.Scripts
             }
         }
 
+        // Test combo   N + N = N, N + H = H, N + S = S, S + S = S, S + H = S, H + H = H
+        private AudioClip getClip(ScriptMaterial other){
+            return null;
+        }
 
     }    
 }
--- VR RoomBend/Assets/Scripts/ScriptMaterial.cs
+++ VR RoomBend/Assets/Scripts/ScriptMaterial.cs
@@ -6,7 +6,6 @@ namespace Assets.Scripts{
     public class ScriptMaterial : MonoBehaviour {
         public string name;
         public AudioClip sound;
-        public int hardness;
 
     }
 }
