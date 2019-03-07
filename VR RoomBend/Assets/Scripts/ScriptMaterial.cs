c37a45f888f57b0e75dbdf29b5ffc6a093d6d901
VeryBester <david15202@gmail.com>
2019-03-07 12:59:06 -0600

    master audio scripts
    -started work on master audio source script
    - finished start function
    - need to work on update/getters/setters

--- VR RoomBend/Assets/Scripts/ScriptMaterial.cs
+++ VR RoomBend/Assets/Scripts/ScriptMaterial.cs
@@ -3,10 +3,12 @@ using UnityEngine;
 
 namespace Assets.Scripts{
     
+ 
     public class ScriptMaterial : MonoBehaviour {
+
         public string name;
         public AudioClip sound;
-
+        public string soundName;
     }
 }
 
