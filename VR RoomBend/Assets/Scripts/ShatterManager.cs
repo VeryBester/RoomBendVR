3e2b539a05f72d7df72ba73aa6faccbc57615ecb
ColeFortson <colemanfortson@192.168.1.11>
2019-03-15 03:20:42 -0500

    Added shatter

---
+++ VR RoomBend/Assets/Scripts/ShatterManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShatterManager : MonoBehaviour
+{
+    public GameObject shatterObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+       Debug.Log("Collider Tag: "+other.collider.tag);
+        if(other.collider.tag == "Floor")
+        {
+            shatterObject.SetActive(true);
+            shatterObject.transform.parent = gameObject.transform.parent;
+            gameObject.SetActive(false);
+        }
+    }
+}
